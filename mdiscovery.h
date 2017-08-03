1a1c7122a97c3e641d79e509a1bd4e4ceba400fa
LWH <960023815@qq.com>
2017-08-03 10:23:07 -0400

    init

---
+++ mdiscovery.h
@@ -0,0 +1,17 @@
+#ifndef __MDISCOVERY_H__
+#define __MDISCOVERY_H__
+
+#ifdef __Cplusplus
+extern .c{
+#endif
+
+
+#define broadcast_net_port    6000
+	
+
+
+#ifdef __Cplusplus
+}
+#endif
+
+#endif
---
+++ mdiscovery_test.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include "mdiscovery.h"
+
+int main(int argc, char ** args)
+{
+	int fd;
+
+	//setvbuf(stdout, NULL, _IONBF, 0);   
+    //fflush(stdout);   
+	if (0 > (fd = mbroadcast_create_fd()))
+	{
+		printf("failed when create fd\n");
+		return -1;
+	}
+
+	if (argc == 2)
+	{
+		while(1)
+		{
+			mbroadcast_send(fd, args[1], strlen(args[1]));
+		}	
+	}
+	else
+	{
+		char buf[512];
+
+		mbroadcast_bind(fd);
+		mbroadcast_reciver(fd, buf, sizeof buf);	
+	}
+
+	return 0;
+}
